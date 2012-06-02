ebde3544f012bf4244cdf461853fb4a07772e205
posaunehm <posaunehm@gmail.com>
2012-06-02 12:31:21 +0900

    TDDBC開始！

---
+++ TDDOsakaTest/VendingMachineTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TDDBCOsaka;
+
+namespace TDDOsakaTest
+{
+    [TestFixture]
+    public class VendingMachineTest
+    {
+        [TestCase]
+        public void お金を投入して金額を確認する()
+        {
+            var venderMachine = new VendorMachine();
+            venderMachine.InsertMoney(100);
+
+            Assert.AreEqual(100, venderMachine.Money);
+        }
+    }
+}
